7ef6df6fe95679c42a500cba4f169108e1aba447
dimavoronkov2222 <122867703+dimavoronkov2222@users.noreply.github.com>
2024-01-15 21:42:01 +0100

    add files

---
+++ Class1.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using GameLibrary;
+namespace GameDatabase
+{
+    public class GameContext : DbContext
+    {
+        public DbSet<Game> Games { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Data Source=LAPTOP-2;Initial Catalog=GameDatabase;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False");
+            }
+        }
+    }
+}
